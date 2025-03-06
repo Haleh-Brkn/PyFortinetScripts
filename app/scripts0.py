c63bf9ee1e211c26f7a72e024468cafa26c6d9c3
Mohammadreza-Tatlari <mohammadreza.tatlari8@gmail.com>
2025-03-06 22:25:43 +0330

    first commit. pyenv and Library are ignored

---
+++ app/scripts0.py
@@ -0,0 +1,26 @@
+from netmiko import Netmiko
+
+fortinet = {
+    'host': '192.168.112.132',
+    'username': 'admin',
+    'password': 'admin',
+    'device_type': 'fortinet'
+}
+
+net_connect= Netmiko(**fortinet)
+print(net_connect.find_prompt())
+command = 'show full-configuration'
+#fullConfigs = net_connect.send_command(command)
+#print(fullConfigs)
+ip_policy_creator= [
+    'config firewall address',
+    'edit IP-180',
+    'set type ipmask',
+    'set subnet 192.168.0.180/32',
+    'end']
+set_config = net_connect.send_config_set(ip_policy_creator) # is an iterable containing all of the configuration commands. The commands will be executed one after the other.
+print("done!")
+print("Log is ",set_config )
+
+
+
